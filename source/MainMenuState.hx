7a2eb6a0eb6faed72431dfe5f3b481c4b5c6fde8
Pasak1094 <98616885+Pasak1094@users.noreply.github.com>
2023-06-29 01:42:39 +0500

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -1,6 +1,5 @@
 package;
 
-import flixel.math.FlxPoint;
 #if desktop
 import Discord.DiscordClient;
 #end
@@ -26,14 +25,12 @@ using StringTools;
 
 class MainMenuState extends MusicBeatState
 {
-	public static var psychEngineVersion:String = '0.5.2h'; //This is also used for Discord RPC
+	public static var psychEngineVersion:String = '0.5.1'; //This is also used for Discord RPC
 	public static var curSelected:Int = 0;
 
 	var menuItems:FlxTypedGroup<FlxSprite>;
 	private var camGame:FlxCamera;
 	private var camAchievement:FlxCamera;
-
-	private var bg:FlxSprite;
 	
 	var optionShit:Array<String> = [
 		'story_mode',
